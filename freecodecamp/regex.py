ea84191af91021d448c4ae0a0d7497fea4bfa1ef
Nicholas-Mercado <Nicholasamerc@gmail.com>
2022-04-07 12:59:23 -0700

    practiced regex

---
+++ freecodecamp/regex.py
@@ -0,0 +1,13 @@
+import re
+
+# re.search() se if string matches regex
+
+# re.findall() extract portions of a string that match a regex
+
+nfile = open('freecodecamp/mbox_s.txt')
+# for line in nfile:
+#   line = line.rstrip()
+#   if re.search('^From:', line):
+#     print(line)
+    
+
